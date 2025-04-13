9253817b917eb00d076f04d5a7a8378560b97dbc
Etoma Egot <etomaa@gmail.com>
2025-04-13 16:38:06 +0000

    1.Modified dim_products by adding country dimension and removing the Case when dimension for Liquidity Category
    2. Added dimension fields to fct_daily_activity
    
    3.Moved previous models to analyses folder

--- models/marts/core/fct_daily_activity.sql
+++ models/marts/core/fct_daily_activity.sql
@@ -7,29 +7,33 @@
 WITH daily_stats AS (
     SELECT
         DATE(created_at) AS activity_date,
+        product_name,
+        market_type,
+        balancingzone,
+        demand_country,
+
         COUNT(*) AS total_demands,
         SUM(recipient_count) AS total_potential_quotes,
         SUM(quotes) AS total_actual_quotes,
-        SUM(quotes) / NULLIF(COUNT(*), 0) AS avg_quotes_per_demand,---compute average quotes per demand incl rows with quotes = NULL or Zero
-        ---AVG(quotes) AS avg_quotes_per_demand_2,  ---compute average quotes per demand excluding rows with quotes = NULL
+        ROUND(SUM(quotes) / NULLIF(COUNT(*), 0), 2) AS avg_quotes_per_demand,
+
         COUNT(DISTINCT author_id) AS active_initiators,
         COUNT(DISTINCT author_company_id) AS active_companies,
-        SUM(volume_in_mwh) AS total_volume_mwh
+
+        ROUND(SUM(volume_in_mwh), 2) AS total_volume_mwh
     FROM {{ ref('int_demand_full') }}
-    GROUP BY DATE(created_at)
+    GROUP BY 
+        activity_date,
+        product_name,
+        market_type,
+        balancingzone,
+        demand_country
 )
 
 SELECT
-    activity_date,
-    total_demands,
-    total_potential_quotes,
-    total_actual_quotes,
-    ROUND(avg_quotes_per_demand,2) AS avg_quotes_per_demand,
-    active_initiators,
-    active_companies,
-    total_volume_mwh,
-    -- Add 7-day rolling averages
-    ROUND(AVG(total_demands) OVER(ORDER BY activity_date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW),2) AS demands_7d_avg,
-    ROUND(AVG(total_actual_quotes) OVER(ORDER BY activity_date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW),2) AS quotes_7d_avg
+    *,
+    -- Rolling 7-day averages (by full platform)
+    ROUND(AVG(total_demands) OVER(PARTITION BY market_type ORDER BY activity_date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS demands_7d_avg,
+    ROUND(AVG(total_actual_quotes) OVER(PARTITION BY market_type ORDER BY activity_date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS quotes_7d_avg
 FROM daily_stats
 ORDER BY activity_date
